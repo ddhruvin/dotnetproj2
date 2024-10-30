a3a37306b081518bbd5326d9d4042c180aaa943e
Dhruvin <120237476+ddhruvin@users.noreply.github.com>
2024-10-30 12:17:47 +0530

    Added Models

---
+++ WebApplication1/Models/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace WebApplication1.Models;
+
+public class Employee
+{
+    public int Id { get; set; }
+    public string FirestName { get; set; }
+    public string MiddleName { get; set; }
+    public string LastName { get; set; }
+    public string EmailAddress { get; set; }
+    public int PhoneNo { get; set; }
+}
