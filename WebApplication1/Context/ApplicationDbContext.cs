5c693e874eab7ba8833fc020e1008ddc9c43dfd8
Dhruvin <120237476+ddhruvin@users.noreply.github.com>
2024-10-30 12:15:34 +0530

    Adding Context

---
+++ WebApplication1/Context/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+namespace company.context;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> contextOptions) : base(contextOptions) { }
+    public DbSet<Employee> Emplyees { get; set; }
+}
